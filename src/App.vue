9a4016e02fffa31ea6d2eb2b467f4a648e81dbbc
V1shal1800 <vishal0018sharma@gmail.com>
2024-11-05 01:04:19 +0530

    add sample changes

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,9 @@
     <Form @key-submitted="onKeySubmit"/>
     <NewCollections :newCollections="newCollections"/>
     <Collections :collections="collections" />
+    <h3>
+      Hello World!
+    </h3>
   </div>
 </template>
 
